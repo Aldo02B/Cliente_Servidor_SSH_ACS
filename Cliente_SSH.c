6129a9eb053d4e6f0559756e86e5a9d7457c46f3
Luis Aldo Gomez Bolaños <aldo.gomez02b@gmail.com>
2024-05-22 20:14:17 -0600

    AVANCE_ALDO
    - Se agregaron los archivos base para el cliente y el servidor.
    Hace falta unir el archivo de la tarea con el servidor para que pueda recibir comandos.

---
+++ Cliente_SSH.c
@@ -0,0 +1,81 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <errno.h>
+ #include <string.h>
+ #include <netdb.h>
+ #include <sys/types.h>
+ #include <netinet/in.h>
+ #include <sys/socket.h>
+ #define MAXDATASIZE 100
+ #define MAXDATASIZE_RESP 20000
+
+ int main(int argc, char *argv[])
+ {
+  // Estos 2 son para el comando
+  char comando[MAXDATASIZE];
+  int len_comando;
+  // Estos 2 son para la respuesta
+  int numbytes;
+  char buf[MAXDATASIZE_RESP];
+
+  int sockfd;  
+  struct hostent *he;
+  struct sockaddr_in cliente; // informacion de la direccion de destino 
+
+  if (argc != 3) {
+    fprintf(stderr,"usage: client hostname puerto\n");
+    exit(1);
+  }
+
+  if ((he=gethostbyname(argv[1])) == NULL) {  // obtener informacion de host servidor 
+   perror("gethostbyname");
+   exit(1);
+  }
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+   perror("socket");
+   exit(1);
+  }
+
+  cliente.sin_family = AF_INET; 
+  cliente.sin_port = htons( atoi(argv[2]) ); 
+  cliente.sin_addr = *((struct in_addr *)he->h_addr);
+  memset(&(cliente.sin_zero), '\0',8);  // poner a cero el resto de la estructura 
+
+  if (connect(sockfd, (struct sockaddr *)&cliente, sizeof(struct sockaddr)) == -1) {
+   perror("connect");
+   exit(1);
+  }
+  
+  /* Se lee el comando del teclado */
+  fgets(comando,MAXDATASIZE-1,stdin);
+  /* Se deja el ultimo caracter vacio */
+  len_comando = strlen(comando) - 1;
+  comando[len_comando] = '\0';
+  /* Se imprime el comando que ingreso el cliente */
+  printf("Comando: %s\n",comando);
+
+  /* Se envia el comando al server */
+  if(send(sockfd,comando, len_comando, 0) == -1) {
+   perror("send()");
+   exit(1);
+  } else 
+   printf("Comando enviado...\n");
+
+  // Si el send no devuelve error continua y lo que falta por hacer
+  // es leer la respuesta
+  if ((numbytes=recv(sockfd, buf, MAXDATASIZE_RESP-1, 0)) == -1) {
+   perror("recv");
+   exit(1);
+  }
+  printf("numbytes=%d\n",numbytes);
+  buf[numbytes] = '\0';
+  printf("Recibido:\n%s\n",buf);
+
+  // Si el recv no devuelve error continua y lo que falta por hacer
+  // es cerrar el file descriptor del cliente
+  close(sockfd);
+
+  return 0;
+} 
